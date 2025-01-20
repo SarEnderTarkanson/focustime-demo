09c86a62dddf5f7d80c47fae4fc068e65243feab
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-01-20 03:35:31 +0100

    initial commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import { Text, SafeAreaView, StyleSheet } from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+
+});
