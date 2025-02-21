b69c63f068d1dc93fe5a967c5b863741edd80ae1
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-02-21 23:51:32 +0100

    added a Rounded Button component

--- src/features/Focus.js
+++ src/features/Focus.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { View, StyleSheet } from "react-native";
 import { TextInput } from "react-native-paper";
+import { RoundedButton } from "../components/RoundedButton";
 
 export const Focus = () => {
   const [subject, setSubject] = useState(null);
@@ -9,9 +10,13 @@ export const Focus = () => {
     <View style={styles.container}>
       <View style={styles.inputContainer}>
         <TextInput
+          style={styles.textInput}
           onChangeText={setSubject}
           label="What would you like to focus on?"
         />
+        <View style={styles.button}>
+          <RoundedButton title="+" size={50} />
+        </View>
       </View>
     </View>
   );
@@ -21,9 +26,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  textInput: {
+    flex: 1,
+    marginRight: 10,
+  },
+  button: {
+    justifyContent: "center",
+  },
   inputContainer: {
-    flex: 0.5,
     padding: 25,
     justifyContent: "top",
+    flexDirection: "row",
   },
 });
